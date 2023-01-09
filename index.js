6ff6b9e9a1e8d3e03b990090d82a3e50e28a082e
Jesse Antonio <jesseantonio1321@gmail.com>
2023-01-09 16:41:57 -0300

    #1 - creating and shuffling deck

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-let deck = [{}];
+let deck = [];
 
 function createGame() {
-
-}
-
-function createDeck() {
-    deck.push()
+    createDeck()
+    shuffleDeck(deck)
+    debugger
 }
 
 function shuffleDeck(deck) {
-
+    deck.sort(() => Math.random() - 0.5);
 }
 
-function createNumberCards(numberCards) {
-    let numberCards = ["A", "2", "3", "4", "5", "6", "7", "10", "11", "12"];
+function createNumberCards() {
+    let numberCards = ["1", "2", "3", "4", "5", "6", "7", "10", "11", "12"];
 
     return numberCards;
 }
 
-function createNumberNaipes() {
-    let numberNaipes = ["♣", "♥", "♠", "♦"];
+function createCardSuits() {
+    let cardsSuits = ["♣", "♥", "♠", "♦"];
+
+    return cardsSuits;
 }
 
-function createCard() {
-    let card = new Object();
-    let numberCards = [];
-    createNumberCards(numberCards);
-    for (i = 0; i < 40; i++) {
-
+function createDeck() {
+    let cardNumbers = createNumberCards();
+    let cardSuits = createCardSuits();
+    for (i = 0; i < 4; i++) {
+        cardNumbers.forEach(number => {
+            let card = new Object();
+            card.number = number;
+            deck.push(card);
+        });
     }
+    let tempDeck = [];
+    cardSuits.forEach(suit => {
+        let tempCardsOfSameSuit = deck.splice(0, 10).map(card => {
+            return {
+                number: card.number,
+                suit: suit
+            };
+        });
+        tempDeck.push(tempCardsOfSameSuit);
+    });
+    deck = tempDeck.flatMap(flat => flat);
 }
