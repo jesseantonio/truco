7dcdff070abe6863c54ca58b22e5c2493dd8507c
Jesse Antonio <jesseantonio1321@gmail.com>
2023-01-09 02:07:21 -0300

    create game

---
+++ index.js
@@ -0,0 +1,32 @@
+let deck = [{}];
+
+function createGame() {
+
+}
+
+function createDeck() {
+    deck.push()
+}
+
+function shuffleDeck(deck) {
+
+}
+
+function createNumberCards(numberCards) {
+    let numberCards = ["A", "2", "3", "4", "5", "6", "7", "10", "11", "12"];
+
+    return numberCards;
+}
+
+function createNumberNaipes() {
+    let numberNaipes = ["♣", "♥", "♠", "♦"];
+}
+
+function createCard() {
+    let card = new Object();
+    let numberCards = [];
+    createNumberCards(numberCards);
+    for (i = 0; i < 40; i++) {
+
+    }
+}
