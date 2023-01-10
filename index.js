dcce611c0bbd172d960d93d74d0815babb4a57a6
Jesse Antonio <jesseantonio1321@gmail.com>
2023-01-10 02:41:42 -0300

    #2 deal the cards and set the manilha

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let deck = [];
 function createGame() {
     createDeck()
     shuffleDeck(deck)
+    dealTheCards()
     debugger
 }
 
@@ -11,7 +12,7 @@ function shuffleDeck(deck) {
 }
 
 function createNumberCards() {
-    let numberCards = ["1", "2", "3", "4", "5", "6", "7", "10", "11", "12"];
+    let numberCards = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
 
     return numberCards;
 }
@@ -37,10 +38,34 @@ function createDeck() {
         let tempCardsOfSameSuit = deck.splice(0, 10).map(card => {
             return {
                 number: card.number,
-                suit: suit
+                suit: suit,
+                isManilha: false,
+                player: {
+                    name: undefined,
+                    points: 0,
+                    roundPoints: 0
+                }
             };
         });
         tempDeck.push(tempCardsOfSameSuit);
     });
     deck = tempDeck.flatMap(flat => flat);
+}
+
+// 3, 2, 1, 12, 11, 10, 7, 6, 5, 4
+function cardStrengthComparator(playerOneCard, playerTwoCard) {
+    if (playerOneCard.number === 3 && playerTwoCard.number != 3) {
+        playerOneCard.player.roundPoints++
+    }
+}
+
+function dealTheCards() {
+    deck.splice(0, 20);
+    for (i = 0; i < 6; i++) {
+        if ((deck.length - 1 - i) % 2 === 0) {
+            deck[deck.length - 1 - i].player.name = "Player 1";
+        } else {
+            deck[deck.length - 1 - i].player.name = "Player 2";
+        }
+    }
 }
