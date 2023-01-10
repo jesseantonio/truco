c6d9f07d219cdd3a4572f9d2fdb47ef2bf7b60bd
Jesse Antonio <jesseantonio1321@gmail.com>
2023-01-10 16:06:35 -0300

    #3 - creating players and init the card comparator

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let deck = [];
+let players = [];
 
 function createGame() {
+    let mode = prompt("Qual será seu modo de jogo? 1v1 ou 2v2?")
+    createPlayers(mode)
+    debugger
     createDeck()
     shuffleDeck(deck)
     dealTheCards()
-    debugger
+    game()
 }
 
 function shuffleDeck(deck) {
@@ -41,9 +45,7 @@ function createDeck() {
                 suit: suit,
                 isManilha: false,
                 player: {
-                    name: undefined,
-                    points: 0,
-                    roundPoints: 0
+                    name: undefined
                 }
             };
         });
@@ -52,20 +54,87 @@ function createDeck() {
     deck = tempDeck.flatMap(flat => flat);
 }
 
-// 3, 2, 1, 12, 11, 10, 7, 6, 5, 4
-function cardStrengthComparator(playerOneCard, playerTwoCard) {
-    if (playerOneCard.number === 3 && playerTwoCard.number != 3) {
-        playerOneCard.player.roundPoints++
-    }
-}
-
 function dealTheCards() {
     deck.splice(0, 20);
+    let last;
     for (i = 0; i < 6; i++) {
         if ((deck.length - 1 - i) % 2 === 0) {
             deck[deck.length - 1 - i].player.name = "Player 1";
         } else {
             deck[deck.length - 1 - i].player.name = "Player 2";
         }
+        last = deck.length - 1 - i;
+    }
+    setManilha(last)
+}
+
+function setManilha(lastCard) {
+    let manilhaIndex = lastCard - 1;
+    let manilha = deck[manilhaIndex].number;
+
+    if (manilha === 7) {
+        manilha = 10;
+    } else if (manilha === 12) {
+        manilha = 1;
+    } else {
+        manilha = manilha + 1;
+    }
+
+    deck.filter(card => card.number === manilha).map(card => card.isManilha = true)
+}
+
+function createPlayers(mode) {
+    if (mode === "1v1") {
+        let player = new Object();
+        player.name = undefined,
+            player.points = undefined,
+            player.roundPoints = undefined
+        for (let i = 0; i < 2; i++) {
+            debugger
+            player.name = `Player ${i + 1}`
+            players.push(player)
+        }
+    } else {
+        for (i = 0; i < 2; i++) {
+            players.push(player)
+        }
+    }
+}
+
+function setRoundPoints(roundPoints) {
+    if (roundPoints + 1 === 2) {
+        deck.filter(el => el.player.name === card.player.name).map(cardEl => cardEl.player.points++);
+    } else {
+        deck.filter(el => el.player.name === card.player.name).map(cardEl => cardEl.player.roundPoints++);
+    }
+}
+
+function round() {
+    let player1 = deck.filter(el => el.player.name === "Player 1");
+    let player2 = deck.filter(el => el.player.name === "Player 2");
+
+    cardStrengthComparator(player1[0], player2[2])
+    debugger
+}
+
+function game() {
+    while (deck.filter(el => el.player.points <= 12).length >= 1) {
+        round()
+    }
+}
+
+// 3, 2, 1, 12, 11, 10, 7, 6, 5, 4
+function cardStrengthComparator(playerOneCard, playerTwoCard) {
+    debugger
+    if (playerOneCard.number === playerTwoCard.number) {
+        console.log("empachou")
+    } else if (playerOneCard.number === 3 && playerTwoCard.number != 3) {
+        setRoundPoints(playerOneRoundPoints)
+    } else if (playerTwoCard.number === 3 && playerOneCard.number != 3) {
+        setRoundPoints(playerTwoRoundPoints)
+    } else if (playerOneCard.number === 2 && playerTwoCard.number != 2) {
+        setRoundPoints(playerOneRoundPoints)
+    } else if (playerTwoCard.number === 2 && playerOneCard.number != 2) {
+        setRoundPoints(playerTwoRoundPoints)
     }
 }
